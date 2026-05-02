1fb98f4650a560ce320c5a4d635fd2463062cbb9
lessurvinz120204 <lessurvinz@gmail.com>
2026-05-02 15:09:52 +0800

    fixed build gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,27 +14,6 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-    
-    afterEvaluate {
-        // 1. Fix for "Namespace not specified"
-        if (project.extensions.findByName("android") != null) {
-            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-            if (android.namespace == null) {
-                android.namespace = project.group.toString()
-            }
-        }
-        
-        // 2. Fix for Kotlin compilation - Set to JVM 11 for library compatibility
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-            compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-        }
-        
-        // 3. Fix for Java compilation - Match with Kotlin (JVM 11)
-        tasks.withType<JavaCompile> {
-            sourceCompatibility = "11"
-            targetCompatibility = "11"
-        }
-    }
 }
 
 subprojects {
